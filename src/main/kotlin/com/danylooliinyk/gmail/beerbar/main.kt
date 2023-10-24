72c463e647bcd7ec35a4e5ee1fb4a4090b2e35bd
Denis Kisarov <denfen123@gmail.com>
2023-10-24 17:03:09 +0300

    added new interface

--- src/main/kotlin/com/danylooliinyk/gmail/beerbar/main.kt
+++ src/main/kotlin/com/danylooliinyk/gmail/beerbar/main.kt
@@ -17,14 +17,20 @@ fun main() {
     val player = BeerDrinker()
     val isInBarPhrase = if (player.isInBar) "В барі" else "Поза баром"
     println("Гравець зараз: $isInBarPhrase")
+    println("Введіть команду:")
+    val userInput = readln()
+    val userInputString = handleCommand(command = userInput)
+    println(userInputString)
 }
 
-fun inputCommand(command: String): String =
-    when (command) {
-        readln("увійти") -> "Зайти в бар"
-        readln("вийти") -> "Команда ще в розробці"
-        readln("сьорб") -> "Команда ще в розробці"
-        readln("риг") -> "Команда ще в розробці"
-        readln("де") -> "Команда ще в розробці"
-        else -> "Такої команди не існує і існувати не буде"
-    }
+fun handleCommand(command: String): String = when (command) {
+    "увійти" -> "Зайти в бар"
+    "вийти" -> "Команда ще в розробці"
+    "сьорб" -> "Команда ще в розробці"
+    "риг" -> "Команда ще в розробці"
+    "де" -> "Команда ще в розробці"
+    else -> "Такої команди не існує і існувати не буде"
+}
+
+
+
