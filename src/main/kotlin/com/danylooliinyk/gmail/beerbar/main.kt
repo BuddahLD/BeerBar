e24b94e2eefb28e34254113c5c47a199475b9d02
Denis Kisarov <denfen123@gmail.com>
2023-10-24 15:55:28 +0300

    added player

--- src/main/kotlin/com/danylooliinyk/gmail/beerbar/main.kt
+++ src/main/kotlin/com/danylooliinyk/gmail/beerbar/main.kt
@@ -7,7 +7,7 @@ fun main() {
     println(
         """
             Команди: 
-            - Зайти в бар -> зайти
+            - Зайти в бар -> увійти
             - Вийти з бару -> вийти
             - Випити пива -> сьорб
             - Відригнути -> риг
@@ -17,5 +17,14 @@ fun main() {
     val player = BeerDrinker()
     val isInBarPhrase = if (player.isInBar) "В барі" else "Поза баром"
     println("Гравець зараз: $isInBarPhrase")
-
 }
+
+fun inputCommand(command: String): String =
+    when (command) {
+        readln("увійти") -> "Зайти в бар"
+        readln("вийти") -> "Команда ще в розробці"
+        readln("сьорб") -> "Команда ще в розробці"
+        readln("риг") -> "Команда ще в розробці"
+        readln("де") -> "Команда ще в розробці"
+        else -> "Такої команди не існує і існувати не буде"
+    }
