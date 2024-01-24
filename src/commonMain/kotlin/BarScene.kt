0032f1be823415e8e5ea36ab0e6b042fbf8cd036
Denis Kisarov <denfen123@gmail.com>
2024-01-24 20:49:36 +0200

    added Bar Scene

---
+++ src/commonMain/kotlin/BarScene.kt
@@ -0,0 +1,9 @@
+import korlibs.korge.scene.Scene
+import korlibs.korge.view.SContainer
+
+class BarScene: Scene() {
+
+    override suspend fun SContainer.sceneMain() {
+        TODO("Not yet implemented")
+    }
+}
