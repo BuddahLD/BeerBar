453e88dc3de5d4121d6a01a47fc2ad2cd281114f
Denis Kisarov <denfen123@gmail.com>
2024-01-31 20:29:02 +0200

    added Bar sides

--- src/commonMain/kotlin/BarScene.kt
+++ src/commonMain/kotlin/BarScene.kt
@@ -1,9 +1,27 @@
+import korlibs.image.color.Colors
 import korlibs.korge.scene.Scene
 import korlibs.korge.view.SContainer
+import korlibs.korge.view.align.centerOnStage
+import korlibs.korge.view.solidRect
 
 class BarScene: Scene() {
 
     override suspend fun SContainer.sceneMain() {
-        TODO("Not yet implemented")
+        val leftSide = solidRect(width = 10, height = 500) {
+            color = Colors.AQUA
+        }
+        val upSide = solidRect(width = 500, height = 10) {
+            color = Colors.AQUA
+        }
+        val rightSide = solidRect(width = 10, height = 500) {
+            color = Colors.AQUA
+        }
+        val bottomLeft = solidRect(width = 240, height = 10) {
+            color = Colors.AQUA
+        }
+        val bottomRight = solidRect(width = 200, height = 10) {
+            color = Colors.AQUA
+        }
+
     }
 }
