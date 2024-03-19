771a84c89d785505b0e6fbe8348a9f1a7c76a18b
Danylo Oliinyk <danylo.oliinyk@gmail.com>
2024-03-19 19:08:22 +0200

    Added stop on walls

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -1,20 +1,17 @@
 
 import korlibs.event.Key
 import korlibs.image.color.Colors
+import korlibs.image.text.RichTextData
 import korlibs.korge.Korge
 import korlibs.korge.input.keys
 import korlibs.korge.scene.Scene
 import korlibs.korge.scene.sceneContainer
-import korlibs.korge.view.SContainer
-import korlibs.korge.view.addUpdater
-import korlibs.korge.view.align.centerOnStage
+import korlibs.korge.view.*
 import korlibs.korge.view.collision.collidesWith
-import korlibs.korge.view.position
-import korlibs.korge.view.solidRect
 import korlibs.math.geom.Size
 
 suspend fun main() = Korge(
-    windowSize = Size(1920, 1080),
+    windowSize = Size(1000, 600),
     backgroundColor = Colors["#2b2b2b"]
 ) {
     sceneContainer {
@@ -25,57 +22,76 @@ suspend fun main() = Korge(
 class InGameScene : Scene() {
 
     private var canMove: Boolean = true
+    private var playerX: Double = 0.0
+    private var playerY: Double = 0.0
 
     override suspend fun SContainer.sceneMain() {
         val beerDrinker = solidRect(width = 64, height = 64) {
             color = Colors.CORNFLOWERBLUE
-            centerOnStage()
+            position(x = 600, y = 250)
         }
 
-        keys {
-            down {
-                if (canMove) {
-                    when (it.key) {
-                        Key.LEFT -> beerDrinker.x -= MOVE_SPEED
-                        Key.RIGHT -> beerDrinker.x += MOVE_SPEED
-                        Key.UP -> beerDrinker.y -= MOVE_SPEED
-                        Key.DOWN -> beerDrinker.y += MOVE_SPEED
-                        else -> Unit
-                    }
-                }
-            }
-        }
-        val leftSide = solidRect(width = 10, height = 500) {
+        val left = solidRect(width = WALL_THICKNESS, height = 500) {
             color = Colors.AQUA
             position(x = 10, y = 0)
         }
-        val upSide = solidRect(width = 500, height = 10) {
+        val top = solidRect(width = 500, height = WALL_THICKNESS) {
             color = Colors.AQUA
             position(x = 10, y = 0)
         }
-        val rightSide = solidRect(width = 10, height = 500) {
+        val right = solidRect(width = WALL_THICKNESS, height = 500) {
             color = Colors.AQUA
             position(x = 500, y = 0)
         }
-        val bottomLeft = solidRect(width = 240, height = 10) {
+        val bottom = solidRect(width = 240, height = WALL_THICKNESS) {
             color = Colors.AQUA
             position(x = 10, y = 500)
         }
-        val bottomRight = solidRect(width = 200, height = 10) {
-            color = Colors.AQUA
-            position(x = 60, y = 500)
+
+        val walls = listOf(left, top, right, bottom)
+        var previousPositionX = 0.0
+        var previousPositionY = 0.0
+
+        keys {
+            down {
+                when (it.key) {
+                    Key.LEFT -> {
+                        previousPositionX = beerDrinker.x
+                        beerDrinker.x -= MOVE_SPEED
+                    }
+                    Key.RIGHT -> {
+                        previousPositionX = beerDrinker.x
+                        beerDrinker.x += MOVE_SPEED
+                    }
+                    Key.UP -> {
+                        previousPositionY = beerDrinker.y
+                        beerDrinker.y -= MOVE_SPEED
+                    }
+                    Key.DOWN -> {
+                        previousPositionY = beerDrinker.y
+                        beerDrinker.y += MOVE_SPEED
+                    }
+                    else -> Unit
+                }
+            }
         }
 
-        beerDrinker.addUpdater {
-            canMove = true
-
-            if (beerDrinker.collidesWith(listOf(leftSide, upSide, rightSide, bottomLeft, bottomRight))) {
-                canMove = false
+        addUpdater {
+            if (beerDrinker.collidesWith(walls)) {
+                beerDrinker.x = previousPositionX
+                beerDrinker.y = previousPositionY
             }
+        }
+
+        textBlock {
+            position(x = 1500, y = 100)
+        }.addUpdater {
+            text = RichTextData(text = "x: $playerX, y: $playerY")
         }
     }
 
     companion object {
-        private const val MOVE_SPEED = 30
+        private const val MOVE_SPEED = 5f
+        private const val WALL_THICKNESS = 20
     }
 }
